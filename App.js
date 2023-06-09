ba0c9282dc5e3fa6ca424ae53a140a8471e0fb72
shomurodoff <shoxruxshomurodoff@gmail.com>
2023-06-09 09:21:28 +0500

    fix:issue

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Router from "./router";
-import { Text, View } from "react-native";
 import { NativeBaseProvider } from "native-base";
 import Query from "./services/query";
 const App = () => {
